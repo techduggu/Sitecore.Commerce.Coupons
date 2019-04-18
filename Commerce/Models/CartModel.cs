0df0fe879998e0952f1cab36d15c14dfb032aa6a
DURGESH KHANDAL <durgeshlive@gmail.com>
2019-04-18 13:38:23 +0530

    Revert "Revert "Add project files.""
    This reverts commit 70440958cd72c7d7c605669edf7303de4207061f.

---
+++ Commerce/Models/CartModel.cs
@@ -0,0 +1,29 @@
+﻿using CommerceServices.Repositories;
+using Sitecore.Commerce.Engine.Connect.Entities;
+using Sitecore.Commerce.Entities.Carts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Commerce.Models
+{
+    [Serializable]
+    public class CartModel
+    {
+        //Bing this property under shoppingcart view to display Total Discount
+        public decimal TotalDiscount { get; set; }
+
+        //Bing this property under shoppingcart view to display Coupon codes applied
+        public IEnumerable<string> PromoCodes { get; set; }
+
+        public void Initialize(CommerceCart cart, CartManager manager)
+        {
+            if (cart.OrderForms != null && cart.OrderForms.Any())
+            {
+                this.PromoCodes = cart.OrderForms.FirstOrDefault().PromoCodes;
+                var cartEntity = (Cart)cart;
+                this.TotalDiscount = (cartEntity.Lines.Sum<CartLine>((Func<CartLine, Decimal>)(lineitem => ((CommerceTotal)lineitem.Total).LineItemDiscountAmount)) + ((CommerceTotal)cartEntity.Total).OrderLevelDiscountAmount);
+            }
+        }
+    }
+}
---
+++ Sample.Plugin.Promotions/Components/CustomPriceInfoComponent.cs
@@ -0,0 +1,12 @@
+﻿using Sitecore.Commerce.Core;
+
+namespace Sample.Plugin.Promotions.Components
+{
+    //This class is created just for reference for this demo. You need to populate and add it to existing cart json.
+    public class CustomPriceInfoComponent : Component
+    {
+        public double MonthlyPrice { get; set; }
+        public double ActivationPrice { get; set; }
+        public double DeliveryPrice { get; set; }
+    }
+}
